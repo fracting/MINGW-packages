5e562cc0fbbd6318f87535ff39c9c08adda9c4fb
Qian Hong <qhong@codeweavers.com>
2016-02-21 23:19:26 +0800

    disable build, just check download (try 2).
    use secure urls
    
    * and follow some redirects
    * prefer http over ftp
    
    temporary disable exit at failure.

--- ci-build.sh
+++ ci-build.sh
@@ -13,7 +13,7 @@ if [[ -t 1 ]]; then
 fi
 status()  { echo -e "\n${cyan}[MSYS2 CI]${normal} ${@}\n"; }
 success() { echo -e "\n${green}[MSYS2 CI] SUCCESS:${normal} ${@}.\n"; exit 0; }
-failure() { echo -e "\n${red}[MSYS2 CI] FAILURE:${normal} ${@}.\n"; exit 1; }
+failure() { echo -e "\n${red}[MSYS2 CI] FAILURE:${normal} ${@}.\n"; echo should exit 1 here; }
 gitconf() { test -n "$(git config ${1})" && return 0; git config --global "${1}" "${2}"; }
 execute() { status "${package:+$package: }${1}"; ${@:2} || failure "${package:+$package: }${1} failed"; }
 
@@ -36,8 +36,8 @@ for package in "${packages[@]}"; do printf "\t${package}\n"; done
 for package in "${packages[@]}"; do
     cd "${package}"
     execute     'Installing dependencies' makepkg-mingw --skippgpcheck --noconfirm --noprogressbar --syncdeps --noprepare --nobuild
-    execute     'Building'                makepkg-mingw --skippgpcheck --noextract --nocheck
-    yes|execute 'Installing'              pacman --upgrade *.pkg.tar.xz
+    #execute     'Building'                makepkg-mingw --skippgpcheck --noextract --nocheck
+    #yes|execute 'Installing'              pacman --upgrade *.pkg.tar.xz
     cd - > /dev/null
 done
 success 'All packages built and installed successfully'
